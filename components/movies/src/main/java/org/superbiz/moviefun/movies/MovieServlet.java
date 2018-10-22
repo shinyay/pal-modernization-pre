1984fc14cfbb207a29eb41507ef5bcb96589fad1
shinyay <shinya.com@gmail.com>
2018-10-22 23:48:59 +0900

    Modify Constructor Injection from @EJB

--- components/movies/src/main/java/org/superbiz/moviefun/movies/MovieServlet.java
+++ components/movies/src/main/java/org/superbiz/moviefun/movies/MovieServlet.java
@@ -16,12 +16,9 @@
  */
 package org.superbiz.moviefun.movies;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
-import org.superbiz.moviefun.movies.Movie;
-import org.superbiz.moviefun.movies.MoviesBean;
+import org.springframework.util.StringUtils;
 
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -39,8 +36,11 @@ public class MovieServlet extends HttpServlet {
 
     public static int PAGE_SIZE = 5;
 
-    @EJB
     private MoviesBean moviesBean;
+
+    public MovieServlet(MoviesBean moviesBean) {
+        this.moviesBean = moviesBean;
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
