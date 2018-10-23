50cc1bca85d87c751b39c72d2d0ab18952cdfb30
shinyay <shinya.com@gmail.com>
2018-10-23 21:32:35 +0900

    Move MovieServlet from Movie Domain to API Component

--- components/movie-fun-api/src/main/java/org/superbiz/moviefun/moviesapi/MovieServlet.java
+++ components/movie-fun-api/src/main/java/org/superbiz/moviefun/moviesapi/MovieServlet.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.superbiz.moviefun.movies;
+package org.superbiz.moviefun.moviesapi;
 
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
