92cfc95cb71693773b47c826e556ca9e6b7ebd38
shinyay <shinya.com@gmail.com>
2018-10-24 15:47:39 +0900

    Add MOVIES_URL as Env Var

--- setenv.fish
+++ setenv.fish
@@ -6,6 +6,8 @@ set -x S3_ENDPOINTURL http://127.0.0.1:9000
 set -x VCAP_SERVICES "{\"aws-s3\": [{\"credentials\": {\"access_key_id\": \"$MINIO_ACCESS_KEY:?missing\", \"bucket\": \"movie-fun-course\", \"secret_access_key\": \"$MINIO_SECRET_ACCESS_KEY:?missing\"}, \"label\": \"aws-s3\", \"name\": \"moviefun-s3\"}]}"
 set -x APPLICATION_OAUTH_ENABLED false
 
+set -x MOVIES_URL http://localhost:8082/movies
+
 set -x EUREKA_CLIENT_ENABLED false
 set -x RIBBON_EUREKA_ENABLED false
 
